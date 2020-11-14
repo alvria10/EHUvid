b9d106c74f97e66fefc6d9d1ed2abd66aacf4b65
alain <alainfedezm@gmail.com>
2020-11-14 18:48:56 +0100

    Implementado horario clases
    Los lunes aparece el horario de los lunes quedan los demás días

--- information.php
+++ information.php
@@ -19,23 +19,7 @@
 
 
 ?>
-<table id="tblHTML"><table style="width:100%">
-                      <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Age</th>
-                      </tr>
-                      <tr>
-                        <td>Jill</td>
-                        <td>Smith</td>
-                        <td>50</td>
-                      </tr>
-                      <tr>
-                        <td>Eve</td>
-                        <td>Jackson</td>
-                        <td>94</td>
-                      </tr>
-                    </table>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -45,8 +29,8 @@
 
          <!--CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/bootstrap.min.css">    
-        <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap" rel="stylesheet">       
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
 
          <!--Mis css-->
@@ -61,11 +45,11 @@
         <script src="js/misjs/calendar.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
 
-        
+
     </head>
     <body class="">
 
-      
+
 
       <!--div principal-->
       <div class="principal row d-flex ">
@@ -81,7 +65,7 @@
 
          <!--Zona alumnado-->
         <div class="col-lg-12 row d-flex align-items-center justify-content-center alumnado">
-    
+
           <!--estado-->
           <div class="col-lg-5 row d-flex  justify-content-center informacion">
             <div class="col-lg-12 row d-flex estado">
@@ -93,17 +77,17 @@
              <!--consultar clases-->
             <div class="col-lg-12 row d-flex consultarclases">
               <h3 class="datos">Consultar clases</h3>
-            
+
             </div>
             <div class="col-lg-12 row d-flex consultarclases">
               <h3 class="datos">Notificaciones</h3>
-            
+
             </div>
 
              <!--notificaciones-->
             <div class="col-lg-9 row d-flex align-items-center  justify-content-center notificaciones">
               <h3 class="datos">Koldo ha cancelado sus clases</h3>
-            
+
             </div>
           </div>
 
@@ -111,20 +95,20 @@
 
           <!--Zona Calendario-->
           <div class="col-lg-7 row d-flex align-items-center justify-content-center calendar">
-           
+
             <div id="calendario">
               <div id="anterior" onclick="mesantes()"></div>
               <div id="posterior" onclick="mesdespues()"></div>
               <h2 id="titulos"></h2>
               <table id="diasc">
                  <!--Dias de la semana-->
-                <tr id="fila0"><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
-                <tr id="fila1"><td onclick="consultarhorario(1,0)"></td><td onclick="consultarhorario(1,1)"></td><td onclick="consultarhorario(1,2)"></td><td onclick="consultarhorario(1,3)"></td><td onclick="consultarhorario(1,4)"></td><td onclick="consultarhorario(1,5)"></td><td onclick="consultarhorario(1,6)"></td></tr>
-                <tr id="fila2"><td onclick="consultarhorario(2,0)"></td><td onclick="consultarhorario(2,1)"></td><td onclick="consultarhorario(2,2)"></td><td onclick="consultarhorario(2,3)"></td><td onclick="consultarhorario(2,4)"></td><td onclick="consultarhorario(2,5)"></td><td onclick="consultarhorario(2,6)"></td></tr>
-                <tr id="fila3"><td onclick="consultarhorario(3,0)"></td><td onclick="consultarhorario(3,1)"></td><td onclick="consultarhorario(3,2)"></td><td onclick="consultarhorario(3,3)"></td><td onclick="consultarhorario(3,4)"></td><td onclick="consultarhorario(3,5)"></td><td onclick="consultarhorario(3,6)"></td></tr>
-                <tr id="fila4"><td onclick="consultarhorario(4,0)"></td><td onclick="consultarhorario(4,1)"></td><td onclick="consultarhorario(4,2)"></td><td onclick="consultarhorario(4,3)"></td><td onclick="consultarhorario(4,4)"></td><td onclick="consultarhorario(4,5)"></td><td onclick="consultarhorario(4,6)"></td></tr>
-                <tr id="fila5"><td onclick="consultarhorario(5,0)"></td><td onclick="consultarhorario(5,1)"></td><td onclick="consultarhorario(5,2)"></td><td onclick="consultarhorario(5,3)"></td><td onclick="consultarhorario(5,4)"></td><td onclick="consultarhorario(5,5)"></td><td onclick="consultarhorario(5,6)"></td></tr>
-                <tr id="fila6"><td onclick="consultarhorario(6,0)"></td><td onclick="consultarhorario(6,1)"></td><td onclick="consultarhorario(6,2)"></td><td onclick="consultarhorario(6,3)"></td><td onclick="consultarhorario(6,4)"></td><td onclick="consultarhorario(6,5)"></td><td onclick="consultarhorario(6,6)"></td></tr>
+                <tr class="emergente0" id="fila0"><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+                <tr class="emergente1" id="fila1"><td  onclick="consultarhorario(1,0)"></td><td onclick="consultarhorario(1,1)"></td><td onclick="consultarhorario(1,2)"></td><td onclick="consultarhorario(1,3)"></td><td onclick="consultarhorario(1,4)"></td><td></td><td></td></tr>
+                <tr class="emergente2" id="fila2"><td onclick="consultarhorario(2,0)"></td><td onclick="consultarhorario(2,1)"></td><td onclick="consultarhorario(2,2)"></td><td onclick="consultarhorario(2,3)"></td><td onclick="consultarhorario(2,4)"></td><td ></td><td ></td></tr>
+                <tr class="emergente3" id="fila3"><td onclick="consultarhorario(3,0)"></td><td onclick="consultarhorario(3,1)"></td><td onclick="consultarhorario(3,2)"></td><td onclick="consultarhorario(3,3)"></td><td onclick="consultarhorario(3,4)"></td><td ></td><td ></td></tr>
+                <tr class="emergente4" id="fila4"><td onclick="consultarhorario(4,0)"></td><td onclick="consultarhorario(4,1)"></td><td onclick="consultarhorario(4,2)"></td><td onclick="consultarhorario(4,3)"></td><td onclick="consultarhorario(4,4)"></td><td ></td><td ></td></tr>
+                <tr class="emergente5" id="fila5"><td onclick="consultarhorario(5,0)"></td><td onclick="consultarhorario(5,1)"></td><td onclick="consultarhorario(5,2)"></td><td onclick="consultarhorario(5,3)"></td><td onclick="consultarhorario(5,4)"></td><td></td><td ></td></tr>
+                <tr class="emergente6" id="fila6"><td onclick="consultarhorario(6,0)"></td><td onclick="consultarhorario(6,1)"></td><td onclick="consultarhorario(6,2)"></td><td onclick="consultarhorario(6,3)"></td><td onclick="consultarhorario(6,4)"></td><td ></td><td></td></tr>
               </table>
               <div id="fechaactual"><i onclick="actualizar()"> </i></div>
               <div id="buscafecha">
@@ -145,29 +129,48 @@
                       <option value="10">Noviembre</option>
                       <option value="11">Diciembre</option>
                     </select>
-               
+
                     <input type="text" name="buscaanno" maxlength="4" size="4" />
-                  
+
                     <input type="button" value="BUSCAR" onclick="mifecha()" />
                   </p>
                 </form>
               </div>
             </div>
-          
+
           </div>
 
-        </div>     
+        </div>
     </div>
- 
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Clases</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" id="horario">
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Exit</button>
+            </div>
+          </div>
+        </div>
+    </div>
+
     <!-- Optional JavaScript -->
- 
+
         <script src="js/jquery.js"></script>
         <script src="js/misjs/calendar.js"></script>
 
         <script src="js/bootstrap.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>  
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script type="text/javascript">
             function consultarhorario(semana, diaSemana){
@@ -180,7 +183,7 @@
                       // $.post("consultaHorario.php",{"texto":miVariableJS},function(respuesta){
                       //     alert(respuesta);
                       // });
-                      if(diaSemana<5){
+                      if(diaSemana<=4){
 
                           if(notmesanterior(semana, diaSemana)){
 
@@ -189,10 +192,20 @@
                              var array = tit.textContent.split(" ");
                              mes = meses.indexOf(array[0]) + 1;
                              dia = celda.textContent;
-                             if (mes > 0){
-                                var tbl = document.getElementById('tblHTML');
-                                alert(tbl.innerHTML);
-                             }
+
+                             //Para que aparezca la ventana emergente con el horario
+                            $(".emergente" + semana).attr("data-toggle" , "modal");
+                            $(".emergente" + semana).attr("data-target" , "#exampleModal");
+                            div = document.getElementById('horario');
+                            div.classList.add('d-flex', 'align-items-center', 'justify-content-center');
+                            div.innerHTML = "";
+
+                            //lunes
+                            if(diaSemana == 0){
+                              document.getElementById("horario").innerHTML = "<div> 13:00 - 15:00 &nbsp;  API &nbsp;  PRESENCIAL &nbsp; <br> 15:00 - 17:00 &nbsp;  TIA &nbsp; ONLINE</div>";
+                            }
+
+
                           }
                       }
                   }
@@ -204,8 +217,5 @@
 
         <!--===============================================================================================-->
         <!--===============================================================================================-->
-
-
-
     </body>
 </html>
